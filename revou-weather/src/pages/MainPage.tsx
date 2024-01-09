0b7ed783f2f6dfec9cd388b6abfd67b49d04a4ed
Fachrezi Ramadhani <ramadhanifachrezi@gmail.com>
2024-01-09 20:41:59 +0700

    modified:   revou-weather/src/pages/MainPage.tsx

--- revou-weather/src/pages/MainPage.tsx
+++ revou-weather/src/pages/MainPage.tsx
@@ -1,10 +1,14 @@
-import { Button, Form, InputGroup, ListGroup } from "react-bootstrap";
+import { Button, Form, InputGroup, } from "react-bootstrap";
 import CityList from "../components/CityList";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-// src/pages/MainPage.tsx
+
+const [searchText, setSearchText] = useState('')
+
 const MainPage = () => {
 
-    const [searchText, setSearchText] = ('')
+    
     return (
       <view>
         <InputGroup className='mb-3'>
@@ -25,9 +29,39 @@ const MainPage = () => {
         </InputGroup>
         <CityList cities={cities} />
       </view>
+
     );
   };
+
+
+  const [cities, setCities] = useState([])
+
+  const callApi = async () => {
+    try {
+      const access_token = '[your token]'
+      const result = await axios.get(`
+        https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?access_token=${access_token}&types=place
+      `)
+
+      setCities(
+        result.data.features.map((city: any) => ({
+          name: city.place_name,
+          lat: city.geometry.coordinates[1],
+          lng: city.geometry.coordinates[0],
+        }))
+      )
+
+    } catch (error) {
+      // if you need to handle any error returned by mapbox
+      console.log(error)
+    }
+  }
   
+  useEffect(() => {
+    callApi()
+  },[searchText])
+  
+
   export default MainPage;
   
   
